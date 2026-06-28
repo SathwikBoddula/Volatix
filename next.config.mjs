0bfc8442820daea54ce03e672e009c2f733ecc8f
Sathwik Boddula <sathwikboddula05@gmail.com>
2026-06-28 13:52:35 +0530

    fix: next config duplicate declaration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-};
-
 export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
